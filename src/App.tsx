81d4d55560edf4fa4d0b5eca71c78e95fb55c013
Mushfiqur Rahman <himel6087@gmail.com>
2025-06-25 19:46:17 +0600

    Add production build to docs/

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Achievements from "./pages/Achievements";
@@ -10,29 +9,28 @@ import Research from "./pages/Research";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import Skills from "./pages/Skills";
+import {
+  HashRouter as Router,   // ← swap BrowserRouter for HashRouter
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/achievements" element={<Achievements />} />
-          <Route path="/research" element={<Research />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/skills" element={<Skills />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <Router>
+    <Routes>
+      <Route path="/" element={<About />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/achievements" element={<Achievements />} />
+      <Route path="/research" element={<Research />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
